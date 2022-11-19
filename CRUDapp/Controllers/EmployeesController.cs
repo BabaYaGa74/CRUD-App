c1885a6a02138b3b80cbf2db8a67bababcc70360
dell <dell@D-E-L-L>
2022-11-19 20:37:44 +0545

    Add project files.

---
+++ CRUDapp/Controllers/EmployeesController.cs
@@ -0,0 +1,104 @@
+﻿using CRUDapp.Data;
+using CRUDapp.Models;
+using CRUDapp.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CRUDapp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext appDbContext;
+
+        public EmployeesController(ApplicationDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employee = await appDbContext.Employees.ToListAsync();
+            return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department,
+            };
+            await appDbContext.Employees.AddAsync(employee);
+            await appDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await appDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id); 
+
+            if(employee != null)
+            {
+                var viewModel = new UpdateViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    DateOfBirth = employee.DateOfBirth,
+                    Department = employee.Department,
+                };
+                return await Task.Run(() => View("View",viewModel));
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateViewModel model)
+        {
+            var employee = await appDbContext.Employees.FindAsync(model.Id);
+
+            if(employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Department = model.Department;
+
+                await appDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateViewModel model)
+        {
+            var employee = await appDbContext.Employees.FindAsync(model.Id);
+            if(employee != null)
+            {
+                appDbContext.Employees.Remove(employee);
+                await appDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}
---
+++ CRUDapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDapp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDapp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
